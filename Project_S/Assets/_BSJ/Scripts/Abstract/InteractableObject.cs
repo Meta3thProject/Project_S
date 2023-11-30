6a7930ab31813f33f0e1883b442bad35c3901e85
BaekSeungJoo <134994626+BaekSeungJoo@users.noreply.github.com>
2023-11-30 11:56:52 +0900

    feat : 상호작용 가능한 아이템 추상 클래스 상속받아서 Trigger체크로 상호작용할 수 있도록 만들었습니다.

---
+++ Project_S/Assets/_BSJ/Scripts/Abstract/InteractableObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 상호 작용한 물체의 추상클래스
+public abstract class InteractableObject : MonoBehaviour
+{
+    // 물체의 체력
+    [field: SerializeField]
+    public int hp {  get; protected set; }
+
+    // 다음 상호작용까지의 시간
+    [field: SerializeField]
+    public float nextInteractionTime { get; protected set; }
+
+    // 다음 상호작용까지의 시간 WaitforSeconds 캐싱
+    protected WaitForSeconds interactTime;
+
+    // 상호 작용 가능한지 조건 체크
+    [field: SerializeField]
+    public bool isInteractionAble { get; protected set; }
+
+    // 물체가 파괴되었을 때 드랍될 아이템
+    [SerializeField]
+    protected GameObject dropedItem;
+
+    protected void Start()
+    {
+        isInteractionAble = true;
+        interactTime = new WaitForSeconds(nextInteractionTime);
+    }
+
+    protected void OnDisable()
+    {
+        isInteractionAble = true;
+    }
+
+    /// <summary>
+    /// 어떻게 상호작용할 것인지에 대한 추상 메서드.
+    /// </summary>
+    public abstract void InteractObject();
+
+    /// <summary>
+    /// 아이템을 드랍하는 함수.
+    /// </summary>
+    public void DropItem()
+    {
+        GameObject _dropItem = Instantiate(dropedItem);
+        _dropItem.transform.position = this.transform.position;
+        this.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 다음 상호작용까지 대기시간을 체크해주는 코루틴.
+    /// </summary>
+    /// <returns></returns>
+    protected IEnumerator NextInteractionTime()
+    {
+        yield return interactTime;
+        isInteractionAble = true;
+        yield return null;
+    }
+}
