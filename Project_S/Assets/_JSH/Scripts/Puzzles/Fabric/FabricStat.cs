b3fa540b8232f38e823a65f703ac198c64934a01
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-12-20 17:41:08 +0900

    FEAT: Fabric puzzle
    옷감 퍼즐 어느정도 완성
    NPC 데이터가 없어서 테스트 불편

---
+++ Project_S/Assets/_JSH/Scripts/Puzzles/Fabric/FabricStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricStat : MonoBehaviour
+{
+    public int fakeID;
+
+    public MBTI target;
+    public int amount;
+}
