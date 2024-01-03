c9688a4a0f7d2cf9dbadb5196d1440e694688374
sh12250 <99242620+sh12250@users.noreply.github.com>
2024-01-03 13:13:52 +0900

    FEAT: NPCs
    각 NPC 스크립트들 작성중

---
+++ Project_S/Assets/_JSH/Scripts/NPC/300000/SuspiciousMan1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspiciousMan1 : NPCBase
+{
+    // 다리 개통: 별 1개 필요
+    public void BridgeOpen()
+    {
+        // TODO: 별 소지수 체크 후 다리 개통
+    }
+
+    public override void PopUpDialog()
+    {
+        base.PopUpDialog();
+    }
+}
---
+++ Project_S/Assets/_JSH/Scripts/NPC/300400/Butler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butler : NPCBase
+{
+    public override void PopUpDialog()
+    {
+        base.PopUpDialog();
+    }
+}
