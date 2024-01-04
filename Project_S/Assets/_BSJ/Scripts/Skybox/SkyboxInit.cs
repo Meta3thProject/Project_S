7b25127c9d5b2b3909d5e26aa2061d56052ff94a
BaekSeungJoo <134994626+BaekSeungJoo@users.noreply.github.com>
2024-01-05 05:23:55 +0900

    feat : 시작할때 스카이 박스의 tiling 크기를 1, 1로 초기화 해주는 로직 추가
    // 퍼즐 위치 재조정
    // +@ 메인 씬에 집 안에 어두운 부분들 라이트 추가로 배치 했습니다.

---
+++ Project_S/Assets/_BSJ/Scripts/Skybox/SkyboxInit.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxInit : MonoBehaviour
+{
+    // 스카이박스 머테리얼
+    Material skyboxMaterial;
+
+    // material tiling 초기값
+    Vector2 initTiling = Vector2.one;
+
+    private void Awake()
+    {
+        skyboxMaterial = GetComponent<Material>();
+        skyboxMaterial.mainTextureScale = initTiling;
+    }
+}
