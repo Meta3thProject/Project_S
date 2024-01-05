1d5fb6e43c2e28ee46bbd468975323e9483e32ee
BaekSeungJoo <134994626+BaekSeungJoo@users.noreply.github.com>
2024-01-05 17:20:05 +0900

    fix : 스타트 버튼 누를 때 플레이어가 이동되지 않는 현상과 메뉴 UI와 충돌나는 부분이 있어서 고쳤습니다.

--- Project_S/Assets/_PJK/Scripts/Menu/MenuButton.cs
+++ Project_S/Assets/_PJK/Scripts/Menu/MenuButton.cs
@@ -15,29 +15,10 @@ public class MenuButton : MonoBehaviour
     public UnityEngine.UI.Button optionbutton;
     public UnityEngine.UI.Button logoutbutton;
     public UnityEngine.UI.Button gameoverbutton;
-    private Text stillplaytext;
-    private Text optiontext;
-    private Text logouttext;
-    private Text gameovertext;
-    private Text stillplaytext2;
-    private Text optiontext2;
-    private Text logouttext2;
-    private Text gameovertext2;
-    private bool isOptionclose;
+
+    private bool isOptionclose = true;
 
 
-
-    private void Start()
-    {
-        stillplaytext = stillplaybutton.GetComponentInChildren<Text>();
-        stillplaytext2 = stillplaybutton.GetComponentInChildren<Text>();
-        optiontext = optionbutton.GetComponentInChildren<Text>();
-        optiontext2 = optionbutton.GetComponentInChildren<Text>();
-        logouttext = logoutbutton.GetComponentInChildren<Text>();
-        logouttext2 = logoutbutton.GetComponentInChildren<Text>();
-        gameovertext = gameoverbutton.GetComponentInChildren<Text>();
-        gameovertext2 = gameoverbutton.GetComponentInChildren<Text>();
-    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.K) || InputBridge.Instance.BackButtonDown)
@@ -56,35 +37,38 @@ public class MenuButton : MonoBehaviour
     public void MenuOpen()
     {
 
+        isOptionclose = false;
         Debug.Log("옵션창꺼져있음");
         Menu.gameObject.SetActive(true);
         firstMenu.gameObject.SetActive(true);
         option.gameObject.SetActive(false);
-        isOptionclose = false;
     }
 
     public void MenuClose()
     {
 
         Debug.Log("옵션창켜져있음");
+        isOptionclose = true;
 
         Menu.gameObject.SetActive(false);
         firstMenu.gameObject.SetActive(false);
         option.gameObject.SetActive(false);
-        isOptionclose = true;
     }
 
 
     public void StillPlay()
     {
-        Debug.Log("계속진행버튼클릭");
+        // BSJ _ 240105
+        return;
 
-        firstMenu.gameObject.SetActive(false);
+        //Debug.Log("계속진행버튼클릭");
 
-        option.gameObject.SetActive(false);
+        //isOptionclose = true;
+        //firstMenu.gameObject.SetActive(false);
 
-        Menu.gameObject.SetActive(false);
-        isOptionclose = true;
+        //option.gameObject.SetActive(false);
+
+        //Menu.gameObject.SetActive(false);
     }
     public void Option()
     {
