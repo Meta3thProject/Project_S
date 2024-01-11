3a6e112982b2b428760d7f2a4996139d484b5609
BaekSeungJoo <134994626+BaekSeungJoo@users.noreply.github.com>
2024-01-11 17:45:35 +0900

    develop : 퍼즐 트리거에 닿으면 해당 퍼즐의 벽이 쳐지는 기능 구현 개발 중 ...

---
+++ Project_S/Assets/_BSJ/Scripts/TransparentWall/PlayerEnterPuzzleTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnterPuzzleTrigger : MonoBehaviour
+{
+    [field: SerializeField] public GameObject puzzleWall { get; private set; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<CharacterController>() != null)
+        {
+            puzzleWall.SetActive(true);
+        }
+    }
+
+    public void RemoveWall()
+    {
+        puzzleWall.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
