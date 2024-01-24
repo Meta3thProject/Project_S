33ced5ce882def2b048d405797a0f7cac0268492
jungkeunpark <134687290+jungkeunpark@users.noreply.github.com>
2024-01-24 14:12:07 +0900

    fix.endingscript
    엔딩스크립트 컬러 mbti글씨체 추가

--- Project_S/Assets/_PJK/Scripts/EndingPosition/EndingAfterTalk+.cs
+++ Project_S/Assets/_PJK/Scripts/EndingPosition/EndingAfterTalk+.cs
@@ -63,97 +63,95 @@ public partial class EndingAfterTalk : MonoBehaviour
     public void MBTIColor(out Color mbticolor)
     {
 
-        string MBTI = default;
+        string MBTI = "ESTJ";
         if (playerstat.GetMBTIStat() != null)
         {
             MBTI = playerstat.GetMBTIStat();
-        }
-        else
-        {
-            MBTI = "INFP";
-        }
 
+        }
         switch (MBTI)
         {
             case "ENFP":
                 mbticolor = new Color(230f / 255f, 50f / 255f, 49f / 255f);
                 endingCanvasTextDetail.text = "당신은 멋있는 그림과 더불어 끝내주는 상상력으로 세계적인 화가가 되었습니다.";
+                endingCanvasTextDetail.color = mbticolor;
 
                 break;
             case "ENTP":
                 mbticolor = new Color(255f / 255f, 128f / 255f, 13f / 255f);
                 endingCanvasTextDetail.text = "당신은 풍부한 호기심과 상상력을 바탕으로 훌륭한 과학자가 되었습니다.";
+                endingCanvasTextDetail.color = mbticolor;
 
                 break;
             case "ENFJ":
                 mbticolor = new Color(116f / 255f, 76f / 255f, 40f / 255f);
                 endingCanvasTextDetail.text = "당신은 멋진 카리스마와 더불어 다른 사람의 잠재력을 파악하는 능력으로 뛰어난 코치가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ENTJ":
                 mbticolor = new Color(9f / 255f, 125f / 255f, 236f / 255f);
                 endingCanvasTextDetail.text = "당신은 남들을 이끄는 리더십과 뛰어난 전략을 토대로 멋진 지휘관이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ESFJ":
                 mbticolor = new Color(186f / 255f, 188f / 255f, 3f / 255f);
                 endingCanvasTextDetail.text = "당신은 다른 사람의 말을 잘 들어주며 다른 사람의 마음을 치료하는 심리상담사가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ESTJ":
                 mbticolor = new Color(8f / 255f, 37f / 255f, 105f / 255f);
                 endingCanvasTextDetail.text = "당신은 논리와 경험을 바탕으로 하는 언변과 결단력으로 뛰어난 변호사가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ESFP":
                 mbticolor = new Color(255f / 255f, 209f / 255f, 18f / 255f);
                 endingCanvasTextDetail.text = "당신은 재치 넘치는 유머 감각을 바탕으로 누구나 알아주는 개그맨이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ESTP":
                 mbticolor = new Color(214f / 255f, 237f / 255f, 0f / 255f);
                 endingCanvasTextDetail.text = "당신은 사그라지지 않는 열정을 바탕으로 뛰어난 소방관이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "INFJ":
                 mbticolor = new Color(180f / 255f, 180f / 255f, 182f / 255f);
                 endingCanvasTextDetail.text = "당신은 타인의 마음을 공감해주는 훌륭한 상담사가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "INTP":
                 mbticolor = new Color(35f / 255f, 24f / 255f, 22f / 255f);
                 endingCanvasTextDetail.text = "당신은 많은 아이디어와 뛰어난 문제 이해력으로 멋진 건축가가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "INFP":
                 mbticolor = new Color(255f / 255f, 255f / 255f, 255f / 255f);
                 endingCanvasTextDetail.text = "당신은 뛰어난 중재력을 바탕으로 무역가가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "INTJ":
                 mbticolor = new Color(130f / 255f, 103f / 255f, 184f / 255f);
                 endingCanvasTextDetail.text = "당신은 훌륭한 판단력을 바탕으로 존경받는 판사가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ISFJ":
                 mbticolor = new Color(253f / 255f, 228f / 255f, 198f / 255f);
                 endingCanvasTextDetail.text = "당신은 뛰어난 지원력을 바탕으로 복지 회사의 사장이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ISTP":
                 mbticolor = new Color(202f / 255f, 186f / 255f, 248f / 255f);
                 endingCanvasTextDetail.text = "당신은 뛰어난 문제 대응력을 바탕으로 멋진 파일럿이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ISFP":
                 mbticolor = new Color(254f / 255f, 207f / 255f, 215f / 255f);
                 endingCanvasTextDetail.text = "당신은 충만한 감성과 이해력으로 세계적인 작곡가가 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             case "ISTJ":
                 mbticolor = new Color(269f / 255f, 226f / 255f, 245f / 255f);
                 endingCanvasTextDetail.text = "당신은 엄청난 집중력과 고도의 침착함으로 훌륭한 조사관이 되었습니다.";
-
+                endingCanvasTextDetail.color = mbticolor;
                 break;
             default:
                 mbticolor = Color.black; // 기본값 설정
